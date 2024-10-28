39eb184210ac38f0d15d66603092f3ac596a1f97
pablo cesar <pablocesar087@gmail.com>
2024-10-28 19:45:40 -0300

    anotation in TS

---
+++ anotations.ts
@@ -0,0 +1,14 @@
+//Annotations are used to describe the type of a variable or function parameter or return value.
+//Helps developers catch errors at compile time.
+
+//Strings
+let myName: string = 'Pablo A.';
+console.log(myName)
+
+//Numbers
+let myNumber: number = 10;
+console.log(myNumber)
+
+//Boolean
+let myBolean: boolean = true;
+console.log(myBolean)
---
+++ index.ts
@@ -0,0 +1,13 @@
+type Person = {
+   name: string;
+   age: number;
+}
+
+const person: Person = {
+   name: "Paul",
+   age: 30
+}
+
+console.log(person.name);
+console.log(person.age);
+
